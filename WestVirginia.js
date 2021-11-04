e3546e4a232c119c4595027835e8635a1cdc1314
distressco36 <93086667+distressco36@users.noreply.github.com>
2021-11-04 15:57:13 -0700

    Update WestVirginia.js

--- WestVirginia.js
+++ WestVirginia.js
@@ -1,16 +1,1 @@
-var WestVirginia = [
-'Charleston, WV'
-,'Huntington, WV'
-,'Morgantown, WV'
-,'Parkersburg, WV'
-,'Wheeling, WV'
-,'Weirton, WV'
-,'Martinsburg, WV'
-,'Fairmont, WV'
-,'Beckley, WV'
-,'Clarksburg, WV'
-,'Teays Valley, WV'
-,'South Charleston, WV'
-,'St. Albans, WV'
-,'Vienna, WV'
-];
+var WestVirginia = ['Charleston, WV','Huntington, WV','Morgantown, WV','Parkersburg, WV','Wheeling, WV','Weirton, WV','Martinsburg, WV','Fairmont, WV','Beckley, WV','Clarksburg, WV','Teays Valley, WV','South Charleston, WV','St. Albans, WV','Vienna, WV'];
