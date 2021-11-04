8793975f818d310e25fd7dfcb7dd3f0b623b375f
distressco36 <93086667+distressco36@users.noreply.github.com>
2021-11-04 10:31:14 -0700

    Create Vermont.js

---
+++ Vermont.js
@@ -0,0 +1,13 @@
+var Vermont = [
+  'Burlington, VT'
+,'Essex, VT'
+,'South Burlington, VT'
+,'Colchester, VT'
+,'Rutland city, VT'
+,'Bennington, VT'
+,'Brattleboro, VT'
+,'Milton, VT'
+,'Hartford, VT'
+,'Essex Junction, VT'
+,'Williston, VT'
+];
