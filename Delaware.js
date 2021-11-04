f0cf5137ebab090e572fc1f851cfa0fc4b36e9a9
distressco36 <93086667+distressco36@users.noreply.github.com>
2021-11-04 10:26:15 -0700

    Create Delaware.js

---
+++ Delaware.js
@@ -0,0 +1,13 @@
+var Delaware = [
+  'Wilmington, DE'
+,'Dover, DE'
+,'Newark, DE'
+,'Middletown, DE'
+,'Bear, DE'
+,'Glasgow, DE'
+,'Brookside, DE'
+,'Hockessin, DE'
+,'Smyrna, DE'
+,'Pike Creek Valley, DE'
+,'Milford, DE'
+];
