17d2423df0b3f836ec42ac43b1b1e85658d346c0
distressco36 <93086667+distressco36@users.noreply.github.com>
2021-11-04 16:03:41 -0700

    Update Vermont.js

--- Vermont.js
+++ Vermont.js
@@ -1,13 +1,1 @@
-var Vermont = [
-  'Burlington, VT'
-,'Essex, VT'
-,'South Burlington, VT'
-,'Colchester, VT'
-,'Rutland city, VT'
-,'Bennington, VT'
-,'Brattleboro, VT'
-,'Milton, VT'
-,'Hartford, VT'
-,'Essex Junction, VT'
-,'Williston, VT'
-];
+var Vermont = ['Burlington, VT','Essex, VT','South Burlington, VT','Colchester, VT','Rutland city, VT','Bennington, VT','Brattleboro, VT','Milton, VT','Hartford, VT','Essex Junction, VT','Williston, VT'];
