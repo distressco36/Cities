9e7f0293b1f20e128da1b55be3bced32dd38c267
distressco36 <93086667+distressco36@users.noreply.github.com>
2021-11-04 10:24:26 -0700

    Create WestVirginia.js

---
+++ WestVirginia.js
@@ -0,0 +1,16 @@
+var WestVirginia = [
+'Charleston, WV'
+,'Huntington, WV'
+,'Morgantown, WV'
+,'Parkersburg, WV'
+,'Wheeling, WV'
+,'Weirton, WV'
+,'Martinsburg, WV'
+,'Fairmont, WV'
+,'Beckley, WV'
+,'Clarksburg, WV'
+,'Teays Valley, WV'
+,'South Charleston, WV'
+,'St. Albans, WV'
+,'Vienna, WV'
+];
