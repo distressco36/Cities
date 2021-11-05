aff110ff508c062b732b38dd645df7ef67bd465f
distressco36 <93086667+distressco36@users.noreply.github.com>
2021-11-05 07:43:03 -0700

    Update Delaware.js

--- Delaware.js
+++ Delaware.js
@@ -1,13 +1,1 @@
-var Delaware = [
-  'Wilmington, DE'
-,'Dover, DE'
-,'Newark, DE'
-,'Middletown, DE'
-,'Bear, DE'
-,'Glasgow, DE'
-,'Brookside, DE'
-,'Hockessin, DE'
-,'Smyrna, DE'
-,'Pike Creek Valley, DE'
-,'Milford, DE'
-];
+var Delaware = ['Wilmington, DE','Dover, DE','Newark, DE','Middletown, DE','Bear, DE','Glasgow, DE','Brookside, DE','Hockessin, DE','Smyrna, DE','Pike Creek Valley, DE','Milford, DE'];
